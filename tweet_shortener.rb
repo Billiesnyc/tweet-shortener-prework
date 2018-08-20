8998c1404267129fe1d795f36a20e569e1ed1a18
Billie Simmons <billiesnyc@gmail.com>
2018-08-20 18:12:14 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,7 +41,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if tweet.length > 140
-    edited_tweet = selective_tweet_shortener(tweet)
+    edited_tweet = bulk_tweet_shortener(tweet)
     final_tweet = edited_tweet.join(" ")
       if final_tweet.length > 140
           puts "#{final_tweet[0..136]}..."

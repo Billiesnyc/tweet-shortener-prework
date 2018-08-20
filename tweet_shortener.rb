d5f1a6e2b941c7180be6873c9f0e68776ea2e294
Billie Simmons <billiesnyc@gmail.com>
2018-08-20 16:44:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split
+  dictionary_array = dictionary.keys
+  replacable_words = dictionary_array & tweet_array
+  dictionary.each do |key, value| tweet_array[key] &&= value
+  
+end
+    
+end

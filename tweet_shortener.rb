a8f2aec4cfb1fdf9817520c89e8a0e3c96ae43bf
Billie Simmons <billiesnyc@gmail.com>
2018-08-20 17:01:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length >= 140
+  if tweet.length > 140
     bulk_tweet_shortener(tweet)
   else 
     tweet 
